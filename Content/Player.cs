4f57276f461303120c1068b80d1eca54ec5db2eb
unknown <antoinepilote1@hotmail.com>
2019-01-03 18:50:46 -0500

    Repositioned player

--- Content/Player.cs
+++ Content/Player.cs
@@ -1,21 +1,73 @@
 using Godot;
 using System;
 
-public class Player : Node
+public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    // Contrainte
+    const float GRAVITY = 20;
+    const float ACCELERATION = 35;
+    const float DECELERATION = 10;
+    const float MAXSPEED = 100;
+    const float STOP_TRESHOLD = 10.1f;
+
+    // Direction de nos Inputs
+    private Vector2 InputDirection = new Vector2();
+
+    // Vitesse de notre joueur
+    private Vector2 Velocity = new Vector2();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInputDirection(); // Obtient la Direction des inputs
+        UpdateVelocity();
+
+        MoveAndSlide(Velocity);
+    }
+
+    private void GetInputDirection()
+    {
+        if (Input.IsActionPressed("ui_left"))
+            InputDirection.x = -1;
+        else if (Input.IsActionPressed("ui_right"))
+            InputDirection.x = 1;
+        else
+            InputDirection.x = 0;
+
+        if (Input.IsActionPressed("ui_up"))
+            InputDirection.y = -1;
+        else if (Input.IsActionPressed("ui_down"))
+            InputDirection.y = 1;
+        else
+            InputDirection.y = 0;
+    }
+
+    private void UpdateVelocity()
+    {
+        // Acceleration
+        Velocity.x += InputDirection.x * ACCELERATION;
+        Velocity.y += InputDirection.y * ACCELERATION;
+
+        // Max Speed
+        if (Mathf.Abs(Velocity.x) > MAXSPEED)
+            Velocity.x = MAXSPEED * Mathf.Sign(Velocity.x);
+        if (Mathf.Abs(Velocity.y) > MAXSPEED)
+            Velocity.y = MAXSPEED * Mathf.Sign(Velocity.y);
+
+        //Deceleration
+        if (InputDirection.x == 0)
+            Velocity.x -= DECELERATION * Mathf.Sign(Velocity.x);
+        if (InputDirection.y == 0)
+            Velocity.y -= DECELERATION * Mathf.Sign(Velocity.y);
+
+        if (Mathf.Abs(Velocity.x) < STOP_TRESHOLD && InputDirection.x == 0)
+            Velocity.x = 0;
+        if (Mathf.Abs(Velocity.y) < STOP_TRESHOLD && InputDirection.y == 0)
+            Velocity.y = 0;
+    }
 }
