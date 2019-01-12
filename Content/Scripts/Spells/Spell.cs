fbcc58bfdecae8abe66ad1ff5aee6d47fdeb037c
Emeryc <emeryc.primeau@gmail.com>
2019-01-12 01:09:44 -0500

    Added Water Spell

---
+++ Content/Scripts/Spells/Spell.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class Spell : Node2D
+{
+    private bool Unlocked = false;
+
+    private string path = "";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
